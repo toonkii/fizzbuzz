3088627a3887a6e72671843b1a1115e1ff5a2e8f
Thomas Clarke <thomas.p.clarke@gmail.com>
2013-07-11 19:45:04 -0400

    Swapped arounf the mod 5 and mod 3 tests.

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
 
     // Count from 1 to 100 if number is a multiple of 3 then output fizz and
     // if a multiple of 5 output buzz and if both a multiple of 3 and 5
-    // outhput fizzbuzz.
+    // output fizzbuzz.
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
 
             if ((i % 3 == 0) && (i % 5 == 0)) {
                 System.out.println("fizzbuzz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
             } else if (i % 3 == 0) {
                 System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
             } else {
                 System.out.println(i);
             }
